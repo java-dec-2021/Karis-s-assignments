d87ae0ee42b1515b0e5b25438a46b36044ba7231
karishans <karishanson@gmail.com>
2021-12-11 22:20:39 -0300

    Bank Account updates

--- 02-java-oop/07-bank-account/BankAccount.java
+++ 02-java-oop/07-bank-account/BankAccount.java
@@ -5,30 +5,39 @@ public class BankAccount{
     public static double totalMoney =0;
 
     public BankAccount(){
-        numOfAccounts++;
+        BankAccount.numOfAccounts+=1;
+        this.checkingBalance =0;
+        this.savingsBalance =0;
 
+    }
+    public double getCheckingBalance() {
+        return this.checkingBalance;
+    }
+    public double getSavingsBalance() {
+        return this.savingsBalance;
     }
     public void depositMoney(String account, double amount){
         if (account == "savings") {
-            this.savingsBalance = this.savingsBalance+amount;
+            // this.savingsBalance = this.savingsBalance+amount;
+            this.savingsBalance +=amount;
             System.out.println("Savings balance: "+this.savingsBalance);
             System.out.println("$"+amount+" deposited in savings account");
-            totalMoney = totalMoney+amount;
         } else if(account =="checking") {
-            checkingBalance = checkingBalance+amount;
+            // this.checkingBalance = this.checkingBalance+amount;
+            this.checkingBalance += amount;
             System.out.println("Checking balance: "+this.checkingBalance);
             System.out.println("$"+amount+" deposited in checking account");
-            totalMoney = totalMoney+amount;
         } else{
             System.out.println("please choose savings or checking option");
         }
-        System.out.println("Checking balance: "+this.checkingBalance);
-        System.out.println("Savings balance: "+this.savingsBalance);
+        // totalMoney = totalMoney+amount;
+        BankAccount.totalMoney += amount;
     }
     public void withdrawMoney(String account, double amount){
         if(account == "savings"){
-            if(this.savingsBalance- amount>0){
-                this.savingsBalance = this.savingsBalance-amount;
+            if(this.savingsBalance- amount>= 0){
+                // this.savingsBalance = this.savingsBalance-amount;
+                this.savingsBalance -= amount;
                 System.out.println("Savings balance: "+this.savingsBalance);
                 System.out.println("$"+amount+" withdrawn from savings account");
                 totalMoney = totalMoney-amount;
@@ -36,8 +45,9 @@ public class BankAccount{
                 System.out.println("Insufficient funds in savings account");
             }
         }else if(account == "checking"){
-            if(this.checkingBalance - amount >0){
-                this.checkingBalance = this.checkingBalance-amount;
+            if(this.checkingBalance - amount >= 0){
+                // this.checkingBalance = this.checkingBalance-amount;
+                this.checkingBalance -= amount;
                 System.out.println("Checking balance: "+this.checkingBalance);
                 System.out.println("$"+amount+" withdrawn from checking account");
                 totalMoney = totalMoney-amount;
@@ -47,18 +57,12 @@ public class BankAccount{
         }else {
             System.out.println("please choose savings or checking option");
         }
-        System.out.println("Checking balance: "+this.checkingBalance);
-        System.out.println("Savings balance: "+this.savingsBalance);
-    }
-    public double getCheckingBalance() {
-        System.out.println(this.checkingBalance);
-        return checkingBalance;
-    }
-    public double getSavingsBalance() {
-        return savingsBalance;
     }
     public double totalAccountsBalance(){
         return checkingBalance+savingsBalance;
+    }
+    public void displayAccountBalance(){
+        System.out.println(String.format("Savings: %.2f, Checking: %.2f",this.savingsBalance, this.checkingBalance));
     }
     public static int numberOfAccounts(){
         return numOfAccounts;
--- 02-java-oop/07-bank-account/BankAccountTest.java
+++ 02-java-oop/07-bank-account/BankAccountTest.java
@@ -1,17 +1,14 @@
 public class BankAccountTest{
     public static void main(String[] args) {
-        BankAccount savings = new BankAccount();
-        BankAccount checking = new BankAccount();
-
-
+        BankAccount bankAccount = new BankAccount();
+        
         System.out.println(BankAccount.numOfAccounts);
 
-        savings.depositMoney("savings", 200);
-        checking.depositMoney("checking", 500);
-        savings.withdrawMoney("savings", 100);
-        checking.withdrawMoney("checking",200);
-        System.out.println(savings.getCheckingBalance());
-        System.out.println(checking.getSavingsBalance());
-        System.out.println(BankAccount.totalMoney);
+        bankAccount.depositMoney("savings", 200);
+        bankAccount.depositMoney("checking", 500);
+        bankAccount.withdrawMoney("savings", 100);
+        bankAccount.withdrawMoney("checking",200);
+        bankAccount.displayAccountBalance();
+        System.out.println(BankAccount.totalValueAccounts());
     }
 }
