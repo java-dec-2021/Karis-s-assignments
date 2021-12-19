fcb6a9b98811a1a6f129045ed9383f7a43d8833c
karishans <karishanson@gmail.com>
2021-12-19 17:43:14 -0300

    hello human updates

--- 03-java-spring/01-spring-fundamentals/03-hello-human/hellohuman/src/main/java/com/karis/hellohuman/controllers/HomeController.java
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/hellohuman/src/main/java/com/karis/hellohuman/controllers/HomeController.java
@@ -1,10 +1,10 @@
 package com.karis.hellohuman.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+@Controller
 public class HomeController {
 
 //	@RequestMapping("/home")
@@ -13,21 +13,33 @@ public class HomeController {
 //	}
 //	
 	@RequestMapping("/")
-	public String name(@RequestParam(value="name", required = false, defaultValue="Human") String name,
-			@RequestParam(value="last_name", required = false, defaultValue="") String lastName) {
-		return "Hello " + name + " " + lastName;
-	}
-	
-	
-	@RequestMapping("/name")
-	public String nameTimes(@RequestParam(value="name") String name, @RequestParam(value="times") int number) {
-		 String nameNum = "";
-		 for(int i = 0; i < number; i++) {
-			 nameNum = nameNum + "Hello " + name + " ";
-		 }
-		 return nameNum;
-		 
+	public String name(
+			@RequestParam(value="name", required = false, defaultValue="Human") String name,
+			@RequestParam(value="last_name", required = false, defaultValue="") String lastName,
+			@RequestParam(value = "times", required = false, defaultValue="") String times,
+			Model model){
+
+		System.out.println(name);
+		System.out.println(lastName);
+		System.out.println(times);
 		
+		model.addAttribute("name", name);
+		model.addAttribute("lastName", lastName);
+		model.addAttribute("times",times);
+		
+		return "index.jsp";
 	}
+	
+//	
+//	@RequestMapping("/name")
+//	public String nameTimes(@RequestParam(value="name") String name, @RequestParam(value="times") int number) {
+//		 String nameNum = "";
+//		 for(int i = 0; i < number; i++) {
+//			 nameNum = nameNum + "Hello " + name + " ";
+//		 }
+//		 return nameNum;
+//		 
+//		
+//	}
 }
 	
